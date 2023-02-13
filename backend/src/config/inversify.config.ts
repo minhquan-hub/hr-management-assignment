7c67a240f948a0e145bc531a595dd3171d8ee129
quantranminh <minhquan2122000@gmail.com>
2023-02-13 12:09:28 +0700

    setup swagger and dependence injection

---
+++ backend/src/config/inversify.config.ts
@@ -0,0 +1,10 @@
+import "reflect-metadata";
+import { Container } from "inversify";
+
+import { TYPES } from "./types";
+
+type T = any;
+
+const container = new Container();
+
+export default container;
